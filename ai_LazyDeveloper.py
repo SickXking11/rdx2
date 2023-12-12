6c4d015e9377a29c981a8edeab5a5f1d6354d864
SickXking11 <94675318+SickXking11@users.noreply.github.com>
2023-12-12 15:10:06 +0530

    Update ai_LazyDeveloper.py

--- ai_LazyDeveloper.py
+++ ai_LazyDeveloper.py
@@ -1,6 +1,6 @@
 #
 #         i am NOT INTRESTED IN BEING YOUR SECOND FATHER... SO DON'T REMOVE MY CREDIT...
-#                                       @LazyDeveloperr
+#                                       
 # 
 # 
 # 
@@ -15,7 +15,7 @@
 #                                                ...PRESENTING...
 #                                  A R T I F i C I A L  -  I N T E L i G E N C E
 #                                              .in  Auto-Filter-Bot.
-#                                                 @LazyDeveloperr
+#                                                 
 #
 #                                       
 #
@@ -30,5 +30,5 @@
 #
 #         this line is for copy-pasters...
 #         ...while you are removing my credit and calling yourself a developerr... 
-#         _____ just imagine, At that time i am fucking your mom and sis at same time, harder & too harder...
+#         _____ ...
 #
